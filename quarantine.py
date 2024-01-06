bf17cb6e95f6f6e4abc5cfe7b7bc2891a0297533
ako652 <akotabe0213@gmail.com>
2024-01-07 00:50:40 +0100

    minor change

--- quarantine.py
+++ quarantine.py
@@ -1,20 +1,25 @@
 import pandas as pd
+import socket
+import platform
+import subprocess
 
 file_path='Quarantine_List.xlsx'
 data=pd.read_excel(file_path)
 data=data.fillna('')
-
-user_input = input("enter the value of PDB GPU OR SWB Manually: ")
+user_start=input('Automatic(A) or Manual(M): ')
+match_found = False
+if user_start == 'M':
+ user_input = input("enter the value of PDB GPU OR SWB Manually: ")
    # Check if the input is a string or numeric value
-if user_input.replace('.', '', 1).isdigit():  # Check if input is numeric
+ if user_input.replace('.', '', 1).isdigit():  # Check if input is numeric
        user_input_num = float(user_input)
        search_value = user_input_num
-else:
+ else:
        user_input_str = user_input
        search_value = user_input_str
    # Search for a match across all cells
-match_found = False
-for row_index, row in data.iterrows():
+
+ for row_index, row in data.iterrows():
        for col_index, value in row.items():
            if isinstance(search_value, float) and isinstance(value, (int, float)) and value == search_value:
                match_found = True
@@ -24,8 +29,22 @@ for row_index, row in data.iterrows():
                break
        if match_found:
            break
+elif user_start=='A':
+    print('this is automatic')
+    hostname=socket.gethostname()
+    output=subprocess.check_output(['uname', '-a'], universal_newlines=True)
+    ip_addr = socket.gethostbyname(hostname)
+    system=platform.system()
+    release=platform.release()
+    version=platform.version()
+    print(f'hostname:{hostname}')
+    print(f'ip_addr: {ip_addr}')
+    print(f'system: {system}')
+    print(f'release: {release}')
+    print(f'version: {version}')
+    print(f'output:{output}')
     
-        
+
         
 if match_found:
     print('\033[91mMatch found!!!!!!!!!!! shut down server and replace \033[0m ' + '' +  user_input)
